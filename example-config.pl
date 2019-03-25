9c7a1cc96368c39cfbda37060093de959e59fa19
Evgenii Lepikhin <johnlepikhin@gmail.com>
2019-03-26 00:01:05 +0300

    Update example

--- example-config.pl
+++ example-config.pl
@@ -1,4 +1,18 @@
 {   destination_directory => '/tmp/guidebook',
     category_system       => 'russian',
     template_directory    => 'templates/template1',
+    lists => {
+             EasyAlaArcha => {
+                               routes_filter  => sub {
+                                   $_->{region} eq 'ala-archa' && $_->{category} le '3Б'
+                               },
+                               route_template => 'route-template1.tex',
+                              },
+             MiddleAlaArcha => {
+                               routes_filter  => sub {
+                                   $_->{region} eq 'ala-archa' && $_->{category} gt '3Б' && $_->{category} lt '5Б'
+                               },
+                               route_template => 'route-template1.tex',
+                              },
+            }
 }
