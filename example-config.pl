9664bad7fe5d8d8cf38207260ab8fbc0168c8793
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2019-04-12 20:40:54 +0300

    List names are now translated to commands names as is

--- example-config.pl
+++ example-config.pl
@@ -2,13 +2,13 @@
     category_system       => 'russian',
     template_directory    => 'templates/template1',
     lists                 => {
-        EasyAlaArcha => {
+        routeListEasyAlaArcha => {
             routes_filter => sub {
                 $_->{region} eq 'ala-archa' && $_->{category} le '3Б';
             },
             route_template => 'route-template1.tex',
         },
-        MiddleAlaArcha => {
+        routeListMiddleAlaArcha => {
             routes_filter => sub {
                 $_->{region} eq 'ala-archa' && $_->{category} gt '3Б' && $_->{category} lt '5Б';
             },
