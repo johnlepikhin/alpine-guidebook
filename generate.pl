658af57fd5ebc8f265ea0ac3dbc3248e24c874fd
Evgenii Lepikhin <johnlepikhin@gmail.com>
2019-04-13 22:28:56 +0300

    Генератор маршрута в отдельной функции, арифметика высот

--- generate.pl
+++ generate.pl
@@ -16,6 +16,7 @@ use Digest::MD5 qw(md5_hex);
 use AG::Config;
 use AG::Categories;
 
+use Syntax::NamedArgs qw(get_arg get_arg_opt);
 use TeX::Processor::Parser;
 use TeX::Processor::Printer;
 use TeX::Processor::Make;
@@ -49,6 +50,137 @@ sub msg {
     }
 }
 
+sub retrieve_geopoints {
+    my $description = get_arg description => @_;
+
+    my @geopoints;
+    TeX::Processor::map_commands(
+        list    => $description,
+        unified => sub {
+            my $command = shift;
+
+            if ( $command->{command} eq 'geopoint' ) {
+                push @geopoints,
+                    {
+                    latitude   => TeX::Processor::Printer::flatten( document => $command->{args}[0]{content} ),
+                    longtitude => TeX::Processor::Printer::flatten( document => $command->{args}[1]{content} ),
+                    altitude   => TeX::Processor::Printer::flatten( document => $command->{args}[2]{content} ),
+                    name       => TeX::Processor::Printer::flatten( document => $command->{args}[4]{content} )
+                        || TeX::Processor::Printer::flatten( document => $command->{args}[3]{content} ),
+                    };
+            }
+
+            return 0
+        } );
+
+    my ( $altitudeMin, $altitudeMax );
+    foreach (@geopoints) {
+        if ( $_->{altitude} eq q{} ) {
+            next;
+        }
+
+        if ( ! defined $altitudeMin ) {
+            $altitudeMin = $_->{altitude};
+            $altitudeMax = $_->{altitude};
+            next;
+        }
+
+        if ( $altitudeMin > $_->{altitude} ) {
+            $altitudeMin = $_->{altitude};
+        }
+        if ( $altitudeMax < $_->{altitude} ) {
+            $altitudeMax = $_->{altitude};
+        }
+    }
+    $altitudeMin //= q{};
+    $altitudeMax //= q{};
+
+    return \@geopoints, $altitudeMin, $altitudeMax;
+}
+
+sub generate_route {
+    my $global   = get_arg global   => @_;
+    my $template = get_arg template => @_;
+    my $route    = get_arg route    => @_;
+
+    my $region = $global->{regions}{ $route->{region} };
+
+    my $geopoints_mapper = sub {
+        my $command = shift;
+
+        if ( my ($geoname) = $command->{command} =~ m{^geo(.+)} ) {
+            if ( exists $region->{geopoints}{$geoname} ) {
+                my $point = $region->{geopoints}{$geoname};
+                my $name  = $point->{name};
+                if ( defined $command->{args} && @{ $command->{args} } && @{ $command->{args}[0]{content} } ) {
+                    $name = TeX::Processor::Printer::latex( document => $command->{args}[0]{content} );
+                }
+                return 1,
+                    TeX::Processor::Make::command(
+                    command => 'geopoint',
+                    args    => [
+                        { type => 'brace', content => [ TeX::Processor::Make::text( sprintf '%.7f', $point->{latitude} ) ], },
+                        { type => 'brace', content => [ TeX::Processor::Make::text( sprintf '%.7f', $point->{longtitude} ) ], },
+                        { type => 'brace', content => [ TeX::Processor::Make::text( $point->{altitude} // q{} ) ], },
+                        { type => 'brace', content => [ TeX::Processor::Make::text($name) ], },
+                        { type => 'brace', content => [ TeX::Processor::Make::text( $point->{name} ) ], },
+                    ] );
+            }
+        }
+
+        return 0;
+    };
+
+    my $description = decode( 'utf-8', read_file("$route->{path}/description.tex") );
+    $description = TeX::Processor::Parser::parse( id => "$route->{path}/description.tex", document => $description );
+
+    # map geopoints
+    $description = TeX::Processor::map_commands(
+        list    => $description,
+        unified => $geopoints_mapper
+    );
+
+    my ($geopoints, $altitudeMin, $altitudeMax) = retrieve_geopoints( description => $description );
+
+    my $category = AG::Categories::get( $global, $route->{category} );
+    my $peaks = join q{, }, @{ $route->{peak} };
+
+    my $uiaa = q{};
+    if ( -f "$route->{path}/uiaa.svg" ) {
+        $uiaa = pdf_of_png( $global, "$route->{path}/uiaa.svg" );
+    }
+
+    my $mapper = sub {
+        my $doc = shift;
+
+        return TeX::Processor::map_commands(
+            list => $doc,
+            map  => {
+                routeTitle       => sub { TeX::Processor::Make::text( $route->{title} ) },
+                routePeak        => sub { TeX::Processor::Make::text($peaks) },
+                routeCategory    => sub { TeX::Processor::Make::text($category) },
+                routeType        => sub { TeX::Processor::Make::text( $route->{type} ) },
+                routeName        => sub { TeX::Processor::Make::text( $route->{name} ) },
+                routeRegionName  => sub { TeX::Processor::Make::text( $region->{name} ) },
+                routeEquipment   => sub { TeX::Processor::Make::text( $route->{equipment} ) },
+                routeAuthors     => sub { TeX::Processor::Make::text( join q{, }, sort @{ $route->{authors} } ) },
+                routeDescription => sub { TeX::Processor::Make::text($description) },
+                routeUIAAPath    => sub { TeX::Processor::Make::text($uiaa) },
+                routeAltitudeMin => sub { TeX::Processor::Make::text($altitudeMin) },
+                routeAltitudeMax => sub { TeX::Processor::Make::text($altitudeMax) },
+            },
+        );
+    };
+
+    $description = $mapper->($description);
+    $description = TeX::Processor::Printer::latex( document => $description );
+
+    my $doc = TeX::Processor::Parser::parse( id => "Route '$route->{title}'", document => $template );
+    $doc = $mapper->($doc);
+
+    return $doc;
+}
+
 binmode STDOUT, ':encoding(utf-8)';
 
 my $global = AG::Config::init();
@@ -69,74 +201,11 @@ while ( my ( $lname, $list ) = each %{ $global->{config}{lists} } ) {
 
     my $content = q{};
     foreach my $route (@list_routes) {
-        my $description = decode( 'utf-8', read_file("$route->{path}/description.tex") );
-        $description = TeX::Processor::Parser::parse( id => "$route->{path}/description.tex", document => $description );
-
-        my $category = AG::Categories::get( $global, $route->{category} );
-        my $peaks = join q{, }, @{ $route->{peak} };
-
-        my $uiaa = q{};
-        if ( -f "$route->{path}/uiaa.svg" ) {
-            $uiaa = pdf_of_png( $global, "$route->{path}/uiaa.svg" );
-        }
-
-        my $region = $global->{regions}{ $route->{region} };
-
-        my $geopoints_mapper = sub {
-            my $command = shift;
-
-            if ( my ($geoname) = $command->{command} =~ m{^geo(.+)} ) {
-                if ( exists $region->{geopoints}{$geoname} ) {
-                    my $point = $region->{geopoints}{$geoname};
-                    my $name  = $point->{name};
-                    if ( defined $command->{args} && @{ $command->{args} } && @{ $command->{args}[0]{content} } ) {
-                        $name = TeX::Processor::Printer::latex( document => $command->{args}[0]{content} );
-                    }
-                    return 1,
-                        TeX::Processor::Make::command(
-                        command => 'geopoint',
-                        args    => [
-                            { type => 'brace', content => [ TeX::Processor::Make::text( sprintf '%.7f', $point->{latitude} ) ], },
-                            { type => 'brace', content => [ TeX::Processor::Make::text( sprintf '%.7f', $point->{longtitude} ) ], },
-                            { type => 'brace', content => [ TeX::Processor::Make::text( $point->{altitude} // q{} ) ], },
-                            { type => 'brace', content => [ TeX::Processor::Make::text($name) ], },
-                            { type => 'brace', content => [ TeX::Processor::Make::text($point->{name}) ], },
-                        ] );
-                }
-            }
-
-            return 0;
-        };
-
-        my $mapper = sub {
-            my $doc = shift;
-
-            return TeX::Processor::map_commands(
-                list => $doc,
-                map  => {
-                    routeTitle       => sub { TeX::Processor::Make::text( $route->{title} ) },
-                    routePeak        => sub { TeX::Processor::Make::text($peaks) },
-                    routeCategory    => sub { TeX::Processor::Make::text($category) },
-                    routeType        => sub { TeX::Processor::Make::text( $route->{type} ) },
-                    routeName        => sub { TeX::Processor::Make::text( $route->{name} ) },
-                    routeRegionName  => sub { TeX::Processor::Make::text( $region->{name} ) },
-                    routeEquipment   => sub { TeX::Processor::Make::text( $route->{equipment} ) },
-                    routeAuthors     => sub { TeX::Processor::Make::text( join q{, }, sort @{ $route->{authors} } ) },
-                    routeDescription => sub { TeX::Processor::Make::text($description) },
-                    routeUIAAPath    => sub { TeX::Processor::Make::text($uiaa) },
-                },
-                unified => $geopoints_mapper
-            );
-        };
-
-        $description = $mapper->($description);
-        $description = TeX::Processor::Printer::latex( document => $description );
-
-        my $doc = $route_template;
-
-        $doc = TeX::Processor::Parser::parse( id => "List $lname, route '$route->{title}'", document => $doc );
-        $doc = $mapper->($doc);
-
+        my $doc = generate_route(
+            global   => $global,
+            template => $route_template,
+            route    => $route,
+        );
         $content .= TeX::Processor::Printer::latex( document => $doc );
     }
 
